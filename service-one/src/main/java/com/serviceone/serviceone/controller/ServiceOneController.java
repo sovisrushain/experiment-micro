74ddde34d81415f3f474d576a9c799a3dde9033f
sovisrushain <sovisrushain@gmail.com>
2023-02-27 14:43:29 +0530

    set 02

--- service-one/src/main/java/com/serviceone/serviceone/controller/ServiceOneController.java
+++ service-one/src/main/java/com/serviceone/serviceone/controller/ServiceOneController.java
@@ -1,16 +1,27 @@
 package com.serviceone.serviceone.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("api/service-one")
 public class ServiceOneController {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{id}")
     public String greet(@PathVariable String id) {
-        return "My favourite show is " + id;
+        String show = webClientBuilder.build()
+                .get()
+                .uri("http://service-two/api/service-two/" + id)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return "My favourite anime is: " + show;
     }
 }
