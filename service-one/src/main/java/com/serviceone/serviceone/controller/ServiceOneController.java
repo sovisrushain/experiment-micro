3a174fa2366286c8796e5083dee63a90dae776da
sovisrushain <sovisrushain@gmail.com>
2023-03-01 16:21:23 +0530

    set 06

--- service-one/src/main/java/com/serviceone/serviceone/controller/ServiceOneController.java
+++ service-one/src/main/java/com/serviceone/serviceone/controller/ServiceOneController.java
@@ -1,5 +1,7 @@
 package com.serviceone.serviceone.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ public class ServiceOneController {
     private WebClient.Builder webClientBuilder;
 
     @GetMapping(value = "/{id}")
+    //@Retry(name = "so-api", fallbackMethod = "fallbackResponse")
+    @CircuitBreaker(name = "so-api", fallbackMethod = "fallbackResponse")
     public String greet(@PathVariable String id) {
         String show = webClientBuilder.build()
                 .get()
@@ -23,5 +27,9 @@ public class ServiceOneController {
                 .bodyToMono(String.class)
                 .block();
         return "My favourite anime is: " + show;
+    }
+
+    public String fallbackResponse(Exception exception) {
+        return "Ops! It seems server is down. Please wait and try again.";
     }
 }
